bfcaf323dcc619db65d2174fbdb6c98befc3e615
FlavioSabino <35316216+FlavioSabino@users.noreply.github.com>
2022-08-26 07:09:42 -0300

    app banco

---
+++ app/src/main/java/com/flavio/banco/Transferencia.kt
@@ -0,0 +1,31 @@
+package com.flavio.banco
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flavio.banco.databinding.ActivityTransferenciaBinding
+
+class Transferencia : AppCompatActivity() {
+
+    private lateinit var binding:ActivityTransferenciaBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTransferenciaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        val toolbar = binding.toolbarTransferencia
+        toolbar.title = "Transferências"
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        toolbar.setTitleMargin(200,0,200,0)
+        toolbar.setBackgroundColor(getColor(R.color.light_blue))
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_voltar))
+        toolbar.setNavigationOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
